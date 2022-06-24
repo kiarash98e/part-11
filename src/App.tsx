6e7aa740e52799a9440c11c0cbe37b8d8db3866f
kiarash98e <kiarashsafaei0@gmail.com>
2022-06-24 15:12:52 +0430

    used package

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React from "react"
+import MagendDrawer from './components/drawer/managed-drawer'
+import Layout from "./components/layout/layout"
+import Search from "./components/search/search";
 
 
-const App:React.FC = () => {
+
+
+const App: React.FC = () => {
+
+  
   return (
-    <div>
-        hello world
-    </div>
+    <>
+      <Layout/>
+      <Search/>
+      <MagendDrawer/>
+    </>
   );
 }
 
