30739d8e8bf325153bfd366c4022ad0aeb63a0c1
kiarash98e <kiarashsafaei0@gmail.com>
2022-06-29 10:53:14 +0430

    /final

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ const MagendDrawer = lazy(() => import('./components/drawer/managed-drawer'))
 const Layout = lazy(() => import("./components/layout/layout")) 
 const Search = lazy(() => import("./components/search/search")) 
 const MagendModal = lazy(() => import("./components/modal/fcModal"))
-
+ 
 
 
 const App: React.FC = () => {
